2da303dd3e01e44bcd64c6eb3d0b17f046d6e25b
MuhireIghor <muhireighor123@gmail.com>
2022-11-27 13:42:04 +0200

    Services component

---
+++ src/components/Services.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { BsCamera } from 'react-icons/bs';
+import { AiOutlineHeart } from 'react-icons/ai';
+import { AiOutlinePicture } from 'react-icons/ai';
+import leaf from '../assets/leaf-blue-ornament-54x64.png';
+
+const Services = () => {
+    return (
+        <div className='flex flex-col  bg-white gap-8 py-20'>
+
+            <div className='flex flex-col items-center justify-center gap-8 '>
+                <img src={leaf} />
+                <p className='text-2xl font-semibold text-[#6b6b6b]'>WE TELL STORIES</p>
+                <p className='text-3xl font-bold text-[#6b6b6b]'>We are here to connect and document your love story the way you want it told</p>
+            </div>
+            <div className='flex flex-row p-12'>
+                <div className='basis-1/3 flex flex-col items-center justify-center gap-4'>
+                    <BsCamera className='text-6xl text-[#d9af98]' />
+                    <p className='text-2xl font-bold text-[#6b6b6b]'>Capturing Weddings</p>
+                    <p className='text-[#6b6b6b] w-3/4'>We make sure we connect with the couple and ensure that the memories that speaks of your love story are preserved. </p>
+                </div>
+                <div className='basis-1/3 flex flex-col items-center justify-center gap-4'>
+                    <AiOutlineHeart className='text-6xl text-[#d9af98]' />
+                    <p className='text-2xl font-bold text-[#6b6b6b]'>Fine Retouching</p>
+                    <p className='text-[#6b6b6b] w-3/4'>Our fine retouching is to ensure each photo and each moments are enhanced, not alter. No moments will be fabricated.    </p>
+                </div>
+                <div className='basis-1/3 flex flex-col items-center justify-center gap-4'>
+                    <AiOutlinePicture className='text-6xl text-[#d9af98]' />
+                    <p className='text-2xl font-bold text-[#6b6b6b]'>Editing Style</p>
+                    <p className='text-[#6b6b6b] w-3/4'>Everything we create is a reflection of the real feelings and the raw interactions during your special day.  </p>
+                </div>
+              
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Services
